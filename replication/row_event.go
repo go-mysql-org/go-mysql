5cf11302f7c7867f2f582c949d23c91c112e3dac
siddontang <siddontang@gmail.com>
2014-12-11 23:09:40 +0800

    refactoring

--- replication/row_event.go
+++ replication/row_event.go
@@ -1,25 +1,99 @@
 package replication
 
+import (
+	"encoding/binary"
+	"fmt"
+	. "github.com/siddontang/go-mysql/mysql"
+	"io"
+)
+
 type TableMapEvent struct {
-	//if post header len == 6
-	//table id is 4bytes, else 6 bytes
-	TableID []byte
+	tableIDSize int
+
+	TableID uint64
 
 	Flags uint16
 
-	SchemaNameLength uint8
-	SchemaName       []byte
-	TableNameLength  uint8
-	TableName        []byte
+	Schema []byte
+	Table  []byte
 
-	//lenenc-int
 	ColumnCount uint64
-	ColumnDef   []byte
-	//lenenc-str
-	ColumnMetaDef []byte
+	ColumnType  []byte
+	ColumnMeta  []uint16
 
-	//len = (ColumnCount + 8) / 7
+	//len = (ColumnCount + 7) / 8
 	NullBitmap []byte
+}
+
+func (e *TableMapEvent) Decode(data []byte) error {
+	pos := 0
+	e.TableID = FixedLengthInt(data[0:e.tableIDSize])
+	pos += e.tableIDSize
+
+	e.Flags = binary.LittleEndian.Uint16(data[pos:])
+	pos += 2
+
+	schemaLength := data[pos]
+	pos++
+
+	e.Schema = data[pos : pos+int(schemaLength)]
+	pos += int(schemaLength)
+
+	//skip 0x00
+	pos++
+
+	tableLength := data[pos]
+	pos++
+
+	e.Table = data[pos : pos+int(tableLength)]
+	pos += int(tableLength)
+
+	//skip 0x00
+	pos++
+
+	var n int
+	e.ColumnCount, _, n = LengthEncodedInt(data[pos:])
+	pos += n
+
+	e.ColumnType = data[pos : pos+int(e.ColumnCount)]
+	pos += int(e.ColumnCount)
+
+	var err error
+	var metaData []byte
+	if metaData, _, n, err = LengthEnodedString(data[pos:]); err != nil {
+		return err
+	}
+
+	if err = e.decodeMeta(metaData); err != nil {
+		return err
+	}
+
+	pos += n
+
+	if len(data[pos:]) != int(e.ColumnCount+7)/8 {
+		return io.EOF
+	}
+
+	e.NullBitmap = data[pos:]
+
+	return nil
+}
+
+func (e *TableMapEvent) decodeMeta(data []byte) error {
+	e.ColumnMeta = make([]uint16, e.ColumnCount)
+	// to do ......
+	// for i, t := range e.ColumnType {
+	// 	switch t {
+
+	// 	}
+	// }
+
+	return nil
+}
+
+func (e *TableMapEvent) Dump(w io.Writer) {
+	fmt.Fprintf(w, "TableID: %d\n", e.TableID)
+
 }
 
 type RowsEvent struct {
