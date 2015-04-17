603e6b006e58d16831ee2df90eafcc7e1c4ff958
siddontang <siddontang@gmail.com>
2015-04-17 11:26:10 +0800

    update canal

--- canal/handler.go
+++ canal/handler.go
@@ -3,40 +3,12 @@ package canal
 import (
 	"errors"
 
-	"github.com/siddontang/go-mysql/schema"
 	"github.com/siddontang/go/log"
 )
 
 var (
 	ErrHandleInterrupted = errors.New("do handler error, interrupted")
 )
-
-const (
-	UpdateAction = "update"
-	InsertAction = "insert"
-	DeleteAction = "delete"
-)
-
-type RowsEvent struct {
-	Table  *schema.Table
-	Action string
-	// changed row list
-	// binlog has three update event version, v0, v1 and v2.
-	// for v1 and v2, the rows number must be even.
-	// Two rows for one event, format is [before update row, after update row]
-	// for update v0, only one row for a event, and we don't support this version.
-	Rows [][]interface{}
-}
-
-func newRowsEvent(table *schema.Table, action string, rows [][]interface{}) *RowsEvent {
-	e := new(RowsEvent)
-
-	e.Table = table
-	e.Action = action
-	e.Rows = rows
-
-	return e
-}
 
 type RowsEventHandler interface {
 	// Handle RowsEvent, if return ErrHandleInterrupted, canal will
