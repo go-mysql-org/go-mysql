e1dd629909a09927b9d237cf91831ad82ae39146
Shlomi Noach <shlomi-noach@github.com>
2016-10-24 09:02:50 +0200

    Conn cleanup after closing (#84)

--- server/command.go
+++ server/command.go
@@ -35,6 +35,7 @@ func (c *Conn) HandleCommand() error {
 	data, err := c.ReadPacket()
 	if err != nil {
 		c.Close()
+		c.Conn = nil
 		return err
 	}
 
@@ -48,6 +49,7 @@ func (c *Conn) HandleCommand() error {
 
 	if err != nil {
 		c.Close()
+		c.Conn = nil
 	}
 	return err
 }
@@ -59,6 +61,7 @@ func (c *Conn) dispatch(data []byte) interface{} {
 	switch cmd {
 	case COM_QUIT:
 		c.Close()
+		c.Conn = nil
 		return noResponse{}
 	case COM_QUERY:
 		if r, err := c.h.HandleQuery(hack.String(data)); err != nil {
