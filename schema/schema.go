916a6cfaa04747916eb9b48a47fe9d0ea0194b10
siddontang <siddontang@gmail.com>
2017-03-11 17:43:40 +0800

    fix canal test

--- schema/schema.go
+++ schema/schema.go
@@ -12,6 +12,8 @@ import (
 	"github.com/siddontang/go-mysql/mysql"
 )
 
+var ErrTableNotExist = errors.New("table is not exist")
+
 const (
 	TYPE_NUMBER    = iota + 1 // tinyint, smallint, mediumint, int, bigint, year
 	TYPE_FLOAT                // float, double
@@ -144,7 +146,26 @@ func (idx *Index) FindColumn(name string) int {
 	return -1
 }
 
+func isTableExist(conn mysql.Executer, schema string, name string) (bool, error) {
+	query := fmt.Sprintf("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '%s' and TABLE_NAME = '%s' LIMIT 1", schema, name)
+	r, err := conn.Execute(query)
+	if err != nil {
+		return false, errors.Trace(err)
+	}
+
+	return r.RowNumber() == 1, nil
+}
+
 func NewTable(conn mysql.Executer, schema string, name string) (*Table, error) {
+	ok, err := isTableExist(conn, schema, name)
+	if err != nil {
+		return nil, errors.Trace(err)
+	}
+
+	if !ok {
+		return nil, ErrTableNotExist
+	}
+
 	ta := &Table{
 		Schema:  schema,
 		Name:    name,
@@ -152,12 +173,12 @@ func NewTable(conn mysql.Executer, schema string, name string) (*Table, error) {
 		Indexes: make([]*Index, 0, 8),
 	}
 
-	if err := ta.fetchColumns(conn); err != nil {
-		return nil, err
+	if err = ta.fetchColumns(conn); err != nil {
+		return nil, errors.Trace(err)
 	}
 
-	if err := ta.fetchIndexes(conn); err != nil {
-		return nil, err
+	if err = ta.fetchIndexes(conn); err != nil {
+		return nil, errors.Trace(err)
 	}
 
 	return ta, nil
