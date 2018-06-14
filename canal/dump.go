b64add046b96e1ab173f8b610ee67e83d1051d1b
Marin <marin@stayke.net>
2018-06-14 08:58:37 -0700

    add header to rows event

--- canal/dump.go
+++ canal/dump.go
@@ -69,7 +69,7 @@ func (h *dumpParseHandler) Data(db string, table string, values []string) error 
 		}
 	}
 
-	events := newRowsEvent(tableInfo, InsertAction, [][]interface{}{vs})
+	events := newRowsEvent(tableInfo, InsertAction, [][]interface{}{vs}, nil)
 	return h.c.eventHandler.OnRow(events)
 }
 
