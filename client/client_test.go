408fef7e872fa374c40c579096ca6482d4cee35c
michael2008 <michael2008@users.noreply.github.com>
2018-08-20 21:29:12 +0800

    fix test instance

--- client/client_test.go
+++ client/client_test.go
@@ -18,7 +18,7 @@ var testHost = flag.String("host", "127.0.0.1", "MySQL server host")
 // starting from MySQL 8.0.4, a new auth plugin is introduced, causing plain password auth to fail with error:
 // ERROR 1251 (08004): Client does not support authentication protocol requested by server; consider upgrading MySQL client
 // Hint: use docker-compose to start corresponding MySQL docker containers and add the their ports here
-var testPort = flag.String("port", "5561,5641,3306,5722,8003,8012,8013", "MySQL server port") // choose one or more form 5561,5641,3306,5722,8003,8012,8013, e.g. '3306,5722,8003'
+var testPort = flag.String("port", "3306", "MySQL server port") // choose one or more form 5561,5641,3306,5722,8003,8012,8013, e.g. '3306,5722,8003'
 var testUser = flag.String("user", "root", "MySQL user")
 var testPassword = flag.String("pass", "", "MySQL password")
 var testDB = flag.String("db", "test", "MySQL test database")
