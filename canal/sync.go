a423548c0768e5bd7be31e9df1ebbe5cf7ff451e
Bulat Aykaev <mefcorvi@gmail.com>
2019-08-20 16:49:30 +0300

    revert 7b99c7952b5c90a054c10ebfb039a7d6dc18934d

--- canal/sync.go
+++ canal/sync.go
@@ -51,7 +51,11 @@ func (c *Canal) runSyncBinlog() error {
 
 		// Update the delay between the Canal and the Master before the handler hooks are called
 		c.updateReplicationDelay(ev)
-
+		// if log pos equal zero ,it is a fake rotate event,ignore it.
+		// see https://github.com/mysql/mysql-server/blob/8cc757da3d87bf4a1f07dcfb2d3c96fed3806870/sql/rpl_binlog_sender.cc#L899
+		if ev.Header.LogPos == 0 {
+			continue
+		}
 		savePos = false
 		force = false
 		pos := c.master.Position()
