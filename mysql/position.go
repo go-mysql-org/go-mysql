1f65914378d708868c5ede7002ac17164a19b7ba
lance6716 <lance6716@gmail.com>
2021-03-03 14:34:25 +0800

    mysql: use numeric comparison for binlog filename

--- mysql/position.go
+++ mysql/position.go
@@ -2,6 +2,9 @@ package mysql
 
 import (
 	"fmt"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 // For binlog filename + position based replication
@@ -12,22 +15,35 @@ type Position struct {
 
 func (p Position) Compare(o Position) int {
 	// First compare binlog name
-	if p.Name > o.Name {
+	nameCmp := CompareBinlogFileName(p.Name, o.Name)
+	if nameCmp != 0 {
+		return nameCmp
+	}
+	// Same binlog file, compare position
+	if p.Pos > o.Pos {
 		return 1
-	} else if p.Name < o.Name {
+	} else if p.Pos < o.Pos {
 		return -1
 	} else {
-		// Same binlog file, compare position
-		if p.Pos > o.Pos {
-			return 1
-		} else if p.Pos < o.Pos {
-			return -1
-		} else {
-			return 0
-		}
+		return 0
 	}
 }
 
 func (p Position) String() string {
 	return fmt.Sprintf("(%s, %d)", p.Name, p.Pos)
+}
+
+func CompareBinlogFileName(a, b string) int {
+	// mysqld appends a numeric extension to the binary log base name to generate binary log file names
+	// ref: https://dev.mysql.com/doc/refman/8.0/en/binary-log.html
+	aNum, _ := strconv.Atoi(strings.TrimLeft(filepath.Ext(a)[1:], "0"))
+	bNum, _ := strconv.Atoi(strings.TrimLeft(filepath.Ext(b)[1:], "0"))
+
+	if aNum > bNum {
+		return 1
+	} else if aNum < bNum {
+		return -1
+	} else {
+		return 0
+	}
 }
