a1fec4210438d01a6a7857ad4fc9681e854c71fc
Tim Vaillancourt <timvaillancourt@github.com>
2021-03-29 20:56:34 +0200

    Handle malformed data in readAuthData

--- server/handshake_resp.go
+++ server/handshake_resp.go
@@ -129,6 +129,11 @@ func (c *Conn) readPluginName(data []byte, pos int) int {
 }
 
 func (c *Conn) readAuthData(data []byte, pos int) ([]byte, int, int, error) {
+	// prevent 'panic: runtime error: index out of range' error
+	if pos > len(data)-1 {
+		return nil, 0, 0, errors.New("bad handshake")
+	}
+
 	// length encoded data
 	var auth []byte
 	var authLen int
