2cffb196613ff81b4417096017fb52c6cf69dd27
Tim Vaillancourt <timvaillancourt@github.com>
2021-03-29 22:16:16 +0200

    Update unit test

--- server/handshake_resp_test.go
+++ server/handshake_resp_test.go
@@ -3,15 +3,15 @@ package server
 import (
 	"testing"
 
-	"github.com/siddontang/go-mysql/mysql"
+	. "github.com/siddontang/go-mysql/mysql"
 )
 
 func TestReadAuthData(t *testing.T) {
 	c := &Conn{
-		capability: mysql.CLIENT_SECURE_CONNECTION,
+		capability: CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA,
 	}
 
-	data := []byte{141, 174, 255, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 114, 111, 111, 116, 0, 20, 28, 152, 53, 72, 150, 120, 94, 151, 3, 104, 218, 30, 186, 82, 221, 123, 12, 50, 148, 88, 109, 121, 115, 113, 108, 95, 112, 101, 114, 102, 0, 109, 121, 115, 113, 108, 95, 110, 97, 116, 105, 118, 101, 95, 112, 97, 115, 115, 119, 111, 114, 100, 0}
+	data := []byte{141, 174, 255, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 114, 111, 111, 116, 0, 20, 190, 183, 72, 209, 170, 60, 191, 100, 227, 81, 203, 221, 190, 14, 213, 116, 244, 140, 90, 121, 109, 121, 115, 113, 108, 95, 112, 101, 114, 102, 0, 109, 121, 115, 113, 108, 95, 110, 97, 116, 105, 118, 101, 95, 112, 97, 115, 115, 119, 111, 114, 100, 0}
 
 	// test out of range index returns 'bad handshake' error
 	_, _, _, err := c.readAuthData(data, len(data))
@@ -20,7 +20,11 @@ func TestReadAuthData(t *testing.T) {
 	}
 
 	// test good index position reads auth data
-	if _, _, _, err := c.readAuthData(data, len(data)-1); err != nil {
+	_, _, readBytes, err := c.readAuthData(data, len(data)-1)
+	if err != nil {
 		t.Fatalf("expected nil error, got %v", err)
+	}
+	if readBytes != len(data)-1 {
+		t.Fatalf("expected %d read bytes, got %d", len(data)-1, readBytes)
 	}
 }
