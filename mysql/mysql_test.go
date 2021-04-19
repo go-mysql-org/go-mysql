707c3e0c52238b6b071e7a9ed1aa82fb10e901ba
lance6716 <lance6716@gmail.com>
2021-04-19 11:52:58 +0800

    fix a bug that not cloned to original memory

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -214,3 +214,13 @@ func (t *mysqlTestSuite) TestMysqlNullDecode(c *check.C) {
 	c.Assert(isNull, check.IsTrue)
 	c.Assert(n, check.Equals, 1)
 }
+
+func (t *mysqlTestSuite) TestMysqlUUIDClone(c *check.C) {
+	us, err := ParseUUIDSet("de278ad0-2106-11e4-9f8e-6edd0ca20947:1-2")
+	c.Assert(err, check.IsNil)
+	c.Assert(us.String(), check.Equals, "de278ad0-2106-11e4-9f8e-6edd0ca20947:1-2")
+
+	clone := us.Clone()
+	c.Assert(clone.String(), check.Equals, "de278ad0-2106-11e4-9f8e-6edd0ca20947:1-2")
+
+}
