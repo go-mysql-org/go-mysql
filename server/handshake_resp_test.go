c35a86a1914b7f677b17213cf0153d780824a39b
atercattus <cat@ater.me>
2021-05-27 19:29:12 +0300

    fix import in test

--- server/handshake_resp_test.go
+++ server/handshake_resp_test.go
@@ -3,12 +3,12 @@ package server
 import (
 	"testing"
 
-	. "github.com/siddontang/go-mysql/mysql"
+	"github.com/go-mysql-org/go-mysql/mysql"
 )
 
 func TestReadAuthData(t *testing.T) {
 	c := &Conn{
-		capability: CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA,
+		capability: mysql.CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA,
 	}
 
 	data := []byte{141, 174, 255, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 114, 111, 111, 116, 0, 20, 190, 183, 72, 209, 170, 60, 191, 100, 227, 81, 203, 221, 190, 14, 213, 116, 244, 140, 90, 121, 109, 121, 115, 113, 108, 95, 112, 101, 114, 102, 0, 109, 121, 115, 113, 108, 95, 110, 97, 116, 105, 118, 101, 95, 112, 97, 115, 115, 119, 111, 114, 100, 0}
