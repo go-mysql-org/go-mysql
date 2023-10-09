f866148e7d6718a8053f47d2e17944c46d718ca0
zing22845 <60884519+zing22845@users.noreply.github.com>
2023-10-09 10:53:53 +0800

    lower memory usage by reducing event chan size in streamer
    When consuming events from stream is blocked, the chan may use too much memory

--- replication/binlogstreamer.go
+++ replication/binlogstreamer.go
@@ -87,7 +87,7 @@ func (s *BinlogStreamer) closeWithError(err error) {
 func NewBinlogStreamer() *BinlogStreamer {
 	s := new(BinlogStreamer)
 
-	s.ch = make(chan *BinlogEvent, 10240)
+	s.ch = make(chan *BinlogEvent, 16)
 	s.ech = make(chan error, 4)
 
 	return s
