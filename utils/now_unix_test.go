19d43944e33a60d217af402afb06ba662fe18f2f
huangjunwei <huangjunwei@youmi.net>
2024-12-28 17:40:09 +0800

    fix within check

--- utils/now_unix_test.go
+++ utils/now_unix_test.go
@@ -19,8 +19,7 @@ func TestCustomTimeNow(t *testing.T) {
 		customTimestamp := Now().UnixNano()
 
 		// two timestamp should within 1 percistion
-		assert.Equal(t, timestamp <= customTimestamp, true, fmt.Sprintf("Loop %d: timestamp <= customTimestamp should be true. timestamp: %d, customTimestamp: %d", i, timestamp, customTimestamp))
-		assert.Equal(t, timestamp+int64(precision) >= customTimestamp, true, fmt.Sprintf("Loop: %d: customTimestamp should within %s. timestamp: %d, customTimestamp: %d", i, precision.String(), timestamp, customTimestamp))
+		assert.Equal(t, timestamp+int64(precision) >= customTimestamp && timestamp-int64(precision) <= customTimestamp, true, fmt.Sprintf("Loop: %d: customTimestamp should within %s. timestamp: %d, customTimestamp: %d", i, precision.String(), timestamp, customTimestamp))
 
 		os.Setenv("TZ", fmt.Sprintf("UTC%d", 14-i%27))
 		time.Sleep(time.Nanosecond)
