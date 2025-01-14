573b1edc0b209ed19386c3703eb334a3dc544087
lance6716 <lance6716@gmail.com>
2025-01-14 13:31:58 +0800

    fix CI
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- mysql/resultset_test.go
+++ mysql/resultset_test.go
@@ -3,8 +3,8 @@ package mysql
 import "testing"
 
 func TestColumnNumber(t *testing.T) {
-	r := Result{}
-	// Make sure ColumnNumber doesn't panic if ResultSet is nil
-	// https://github.com/go-mysql-org/go-mysql/issues/964
+	r := NewResultReserveResultset(0)
+	// Make sure ColumnNumber doesn't panic when constructing a Result with 0
+	// columns. https://github.com/go-mysql-org/go-mysql/issues/964
 	r.ColumnNumber()
 }
