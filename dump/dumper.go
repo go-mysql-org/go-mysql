e7abb6c63bcb9299efb1c81f8f956f70d13bf1b0
Daniël van Eeden <git@myname.nl>
2025-03-10 08:32:42 +0100

    dump: Fix Unlick typo

--- dump/dumper.go
+++ dump/dumper.go
@@ -15,7 +15,7 @@ import (
 	"github.com/pingcap/errors"
 )
 
-// Unlick mysqldump, Dumper is designed for parsing and syning data easily.
+// Unlike mysqldump, Dumper is designed for parsing and syning data easily.
 type Dumper struct {
 	// mysqldump execution path, like mysqldump or /usr/bin/mysqldump, etc...
 	ExecutionPath string
