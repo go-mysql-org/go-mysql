fdfe0b26e1b8a74e028c43bfa0ebbe0fa18887ae
Daniël van Eeden <github@myname.nl>
2025-04-01 10:38:29 +0200

    Update server/conn.go
    Co-authored-by: ccoVeille <3875889+ccoVeille@users.noreply.github.com>

--- server/conn.go
+++ server/conn.go
@@ -38,8 +38,9 @@ type Conn struct {
 
 var baseConnID uint32 = 10000
 var defaultServer *Server
-
-// NewConn: create connection with default server settings
+ // NewConn: create connection with default server settings
+//
+// Deprecated: Use Server.NewConn instead.
 // Deprecated: Use Server.NewConn instead.
 func NewConn(conn net.Conn, user string, password string, h Handler) (*Conn, error) {
 	if defaultServer == nil {
