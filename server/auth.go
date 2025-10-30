90dbffc6641fa5b08b2d1956127ae1d8b727dfac
Kim Nielsen <github@insecurity.dk>
2025-10-30 07:33:18 +0100

    Make authentication an interface for easy customization (#1062)
    Co-authored-by: lance6716 <lance6716@gmail.com>

--- server/auth.go
+++ server/auth.go
@@ -27,36 +27,7 @@ func (c *Conn) compareAuthData(authPluginName string, clientAuthData []byte) err
 		return c.handleAuthSwitchResponse()
 	}
 
-	switch authPluginName {
-	case mysql.AUTH_NATIVE_PASSWORD:
-		return c.compareNativePasswordAuthData(clientAuthData, c.credential)
-
-	case mysql.AUTH_CACHING_SHA2_PASSWORD:
-		if !c.cachingSha2FullAuth {
-			if err := c.compareCacheSha2PasswordAuthData(clientAuthData); err != nil {
-				return err
-			}
-			if c.cachingSha2FullAuth {
-				return c.handleAuthSwitchResponse()
-			}
-			return nil
-		}
-		// AuthMoreData packet already sent, do full auth
-		return c.handleCachingSha2PasswordFullAuth(clientAuthData)
-
-	case mysql.AUTH_SHA256_PASSWORD:
-		cont, err := c.handlePublicKeyRetrieval(clientAuthData)
-		if err != nil {
-			return err
-		}
-		if !cont {
-			return nil
-		}
-		return c.compareSha256PasswordAuthData(clientAuthData, c.credential)
-
-	default:
-		return errors.Errorf("unknown authentication plugin name '%s'", authPluginName)
-	}
+	return c.serverConf.authProvider.Authenticate(c, authPluginName, clientAuthData)
 }
 
 func (c *Conn) acquirePassword() error {
