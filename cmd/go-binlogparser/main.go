2b18c3ecd2007775c585b203bf1c0366d1cd4840
Daniël van Eeden <git@myname.nl>
2026-02-07 10:56:17 +0100

    More checksum improvements

--- cmd/go-binlogparser/main.go
+++ cmd/go-binlogparser/main.go
@@ -10,12 +10,14 @@ import (
 var (
 	name   = flag.String("name", "", "binlog file name")
 	offset = flag.Int64("offset", 0, "parse start offset")
+	verify = flag.Bool("verify", false, "verify checksum")
 )
 
 func main() {
 	flag.Parse()
 
 	p := replication.NewBinlogParser()
+	p.SetVerifyChecksum(*verify)
 
 	f := func(e *replication.BinlogEvent) error {
 		e.Dump(os.Stdout)
